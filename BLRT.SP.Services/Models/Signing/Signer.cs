e06aead1474894fb1721c933b7c5e3fe674d8b30
bibily <an.dmitrijeva@gmail.com>
2021-10-01 13:20:50 +0300

    signer model

---
+++ BLRT.SP.Services/Models/Signing/Signer.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+
+namespace BLRT.SP.Services.Models.Signing
+{
+    [DataContract]
+    public class Signer
+    {
+        [DataMember(IsRequired = false)]
+        public string FirstName { get; set; }
+        [DataMember(IsRequired = false)]
+        public string LastName { get; set; }
+        [DataMember(IsRequired = false)]
+        public string EMail { get; set; }
+        [DataMember(IsRequired = false)]
+        public string Role { get; set; }
+        [DataMember(IsRequired = false)]
+        public string Code { get; set; }
+        [DataMember(IsRequired = false)]
+        public string Country { get; set; }
+        [DataMember(IsRequired = false)]
+        public string Company { get; set; }
+        [DataMember(IsRequired = false)]
+        public string Position { get; set; }
+        [DataMember(IsRequired = false)]
+        public string Phone { get; set; }
+        [DataMember(IsRequired = false)]
+        public string Token { get; set; }
+    }
+}
